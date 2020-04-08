bab9e9b507c52f5b6589d000229707b18d7cd25a
mgonawala <mohini.gonawala90@gmail.com>
2020-04-08 16:31:23 +0530

    Change docker image tag to latest

--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -2,10 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://reverseproxy:8080/api/v0'
+  apiHost: "http://localhost:8080/api/v0"
 };
 
 /*
