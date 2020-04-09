c2b7a3a6664b74893820e00f558ca059cb7f9aad
mgonawala <mohini.gonawala90@gmail.com>
2020-04-09 22:02:23 +0530

    Change ELB URL

--- udacity-c3-frontend/src/environments/environment.ts
+++ udacity-c3-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: "http://a3d3e562b79cb11ea9c65026974534cd-2040409207.us-east-1.elb.amazonaws.com:8080/api/v0"
+  apiHost: "http://a3d3e562b79cb11ea9c65026974534cd-2040409207.us-east-1.elb.amazonaws.coml:8080/api/v0"
 };
 
 /*
