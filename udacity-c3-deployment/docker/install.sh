6042b16c55888979cf88aaf5965397aa06032603
mgonawala <mohini.gonawala90@gmail.com>
2020-04-09 19:02:45 +0530

    Build & deploy frontend service

---
+++ udacity-c3-deployment/docker/install.sh
@@ -0,0 +1,41 @@
+$(git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep ../../udacity-c3-restapi-feed/Dockerfile > /dev/null)
+dockerfile_status=$?
+
+if [[ ${dockerfile_status} == "0" ]]
+then
+    docker build mohinigonawala90/backend-feed:latest ../../udacity-c3-restapi-feed/Dockerfile;
+    export BUILD_FEED=true;
+    docker tag $DOCKER_USER/backend-feed:latest $DOCKER_USER/backend-feed:${TRAVIS_BUILD_NUMBER};
+    docker push $DOCKER_USER/backend-feed:${TRAVIS_BUILD_NUMBER};
+    docker push $DOCKER_USER/backend-feed:latest;
+else
+    echo "New docker image for feed is not required";
+fi
+
+$(git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep ../../udacity-c3-restapi-user/Dockerfile > /dev/null)
+dockerfile_status=$?
+
+if [[ ${dockerfile_status} == "0" ]]
+then
+    docker build mohinigonawala90/backend-user:latest ../../udacity-c3-restapi-user/Dockerfile;
+      export BUILD_USER=true;
+        docker tag $DOCKER_USER/backend-user:latest $DOCKER_USER/backend-user:${TRAVIS_BUILD_NUMBER};
+          docker push $DOCKER_USER/backend-user:${TRAVIS_BUILD_NUMBER};
+          docker push $DOCKER_USER/backend-user:latest;
+else
+    echo "New docker image for user is not required";
+fi
+
+$(git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep ../../udacity-c3-frontend/Dockerfile > /dev/null)
+dockerfile_status=$?
+
+if [[ ${dockerfile_status} == "0" ]]
+then
+    docker build mohinigonawala90/backend-feed:latest ../../udacity-c3-frontend/Dockerfile;
+      export BUILD_FRONTEND=true;
+      docker tag $DOCKER_USER/frontend:latest $DOCKER_USER/frontend:${TRAVIS_BUILD_NUMBER};
+                docker push $DOCKER_USER/frontend:${TRAVIS_BUILD_NUMBER};
+                docker push $DOCKER_USER/frontend:latest;
+else
+    echo "New docker image for frontend is not required";
+fi
